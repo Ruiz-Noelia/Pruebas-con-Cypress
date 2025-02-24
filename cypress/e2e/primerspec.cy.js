8f035a91ff99a13bd5f78b55c23cca1f241f0938
Noelia Ruiz <sagitrinidad@gmail.com>
2025-02-23 21:23:54 -0300

    Instalación y primer prueba (spec) ejecutada con Cypress

---
+++ cypress/e2e/primerspec.cy.js
@@ -0,0 +1,5 @@
+describe('template spec', () => { //describe es la suite
+  it('passes', () => { //nombre de la prueba
+    cy.visit('https://36dev.bimtrazer.com') //es el comando que va a ejecutar
+  })
+})
