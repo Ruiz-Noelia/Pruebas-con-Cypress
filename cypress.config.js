9b2ac99d67d7864e2addf3a4c81d8733f55effae
Ruiz noelia <sagitrinidad@gmail.com>
2025-02-26 02:29:42 -0300

    Se añade archivo -json con los datos para login de usuarios -saucedemo- y se realizar la primer prueba -usuario bloqueado-

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    experimentalStudio:true, //lo habilitamos para grabar pruebas y poder capturar selectores
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
 
