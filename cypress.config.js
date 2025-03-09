df455240a5d340d4a2b5922ea8651e40c134df23
Noelia Ruiz <Ruiz Noelia>
2025-03-08 22:34:30 -0300

    Configuramos cypress para generar videos en las pruebas que realizamos, generamos captura de muestra

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     baseUrl: "https://www.saucedemo.com/v1/", //se define la url de base que se va a visitar en las pruebas que se hagan
     experimentalStudio: true, //lo habilitamos para grabar pruebas y poder capturar selectores
     reporter: 'cypress-mochawesome-reporter', //importamos la nueva librería para generar los reportes
-
+    video: false, // lo dejo falso para que no me guarde videos de las pruebas que haga
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
