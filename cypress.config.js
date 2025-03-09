1ec5396e0527a31891ad2b0a9511f6f8d06da0e9
Noelia Ruiz <Ruiz Noelia>
2025-03-08 21:39:24 -0300

    Se instala liberia -mochawesome- y se configura cypress para correr comandos por consola, se añade captura de ejecución

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,13 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://www.saucedemo.com/v1/", //se define la url de base que se va a visitar en las pruebas que se hagan
-    experimentalStudio:true, //lo habilitamos para grabar pruebas y poder capturar selectores
+    experimentalStudio: true, //lo habilitamos para grabar pruebas y poder capturar selectores
+    reporter: 'cypress-mochawesome-reporter', //importamos la nueva librería para generar los reportes
+
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on); //hacemos que desde end 2 end se escuchen los eventos
 
       //archivo de configuración
     },
