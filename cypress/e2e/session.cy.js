da55bcb97f8ba18c0e0ff43674a6bd3ce0cf8d0f
Ruiz-Noelia <sagitrinidad@gmail.com>
2025-03-16 19:13:25 -0300

    Segunda parte de -sessions- agregamos código de error al cargar la url de Saucedemo, la demora ahora de las pruebas es de 3 segundos - se añaden capturas de tests exitosos

--- cypress/e2e/session.cy.js
+++ cypress/e2e/session.cy.js
@@ -1,19 +1,27 @@
 describe('sessions en Cypress', () => {
-    
+
     beforeEach(() => {
-        cy.visit('/')
-        cy.loginOk2()
+        //antes de cada prueba iniciamos la sesión
+        cy.session('Login Saucedemo', () => {
+            cy.visit('/')
+            cy.loginOk2()
+        })
+
     })
 
     it('verificamos la url', () => {
+        cy.visit('/inventory.html', {failOnStatusCode: false}) //se añade el código de error en la página de SauceDemo
         cy.url().should('contain', '/inventory.html')
     });
 
     it('verificar título', () => {
+        cy.visit('/inventory.html', {failOnStatusCode: false})
         cy.title().should('eq', 'Swag Labs')
     });
 
     it('verificar primer producto - mochila', () => {
+        cy.visit('/inventory.html', {failOnStatusCode: false})
+
         cy.get('#item_4_title_link > div').contains('Sauce Labs Backpack').click()
         cy.url().should('contain', '/inventory-item.html?id=4')
         cy.asertion_text('[data-test="inventory-item-name"]', 'Sauce Labs Backpack')
@@ -21,7 +29,7 @@ describe('sessions en Cypress', () => {
         cy.get('[data-test="inventory-item-desc"]').should('contain', 'carry.allTheThings()')
         cy.get('[data-test="inventory-item-price"]').should('contain', '$29.99')
         cy.get('[data-test="add-to-cart"]').click()
-        cy.get('[data-test="shopping-cart-link"]').should('contain' ,'1')
+        cy.get('[data-test="shopping-cart-link"]').should('contain', '1')
         cy.get('[data-test="remove"]').click()
 
 
@@ -32,6 +40,8 @@ describe('sessions en Cypress', () => {
 
 
     it('verificar segundo producto - remera', () => {
+        cy.visit('/inventory.html', {failOnStatusCode: false})
+
         cy.get('[data-test="item-1-title-link"] > [data-test="inventory-item-name"]').contains('Sauce Labs Bolt T-Shirt').click()
         cy.url().should('contain', '/inventory-item.html?id=1')
         cy.asertion_text('[data-test="inventory-item-name"]', 'Sauce Labs Bolt T-Shirt')
@@ -39,7 +49,7 @@ describe('sessions en Cypress', () => {
         cy.get('[data-test="inventory-item-desc"]').should('contain', 'Get your testing superhero on with the Sauce Labs bolt T-shirt. From American Apparel, 100% ringspun combed cotton, heather gray with red bolt.')
         cy.get('[data-test="inventory-item-price"]').should('contain', '$15.99')
         cy.get('[data-test="add-to-cart"]').click()
-        cy.get('[data-test="shopping-cart-link"]').should('contain' ,'1')
+        cy.get('[data-test="shopping-cart-link"]').should('contain', '1')
         cy.get('[data-test="remove"]').click()
 
 
@@ -47,9 +57,11 @@ describe('sessions en Cypress', () => {
         cy.get('#shopping_cart_container > a > span').should('not.exist');
         /* ==== End Cypress Studio ==== */
     });
-    
+
 
     it('verificar tercer producto - luz de bici', () => {
+        cy.visit('/inventory.html', {failOnStatusCode: false})
+
         cy.get('[data-test="item-0-title-link"] > [data-test="inventory-item-name"]').contains('Sauce Labs Bike Light').click()
         cy.url().should('contain', '/inventory-item.html?id=0')
         cy.asertion_text('[data-test="inventory-item-name"]', 'Sauce Labs Bike Light')
@@ -57,7 +69,7 @@ describe('sessions en Cypress', () => {
         cy.get('[data-test="inventory-item-desc"]').should('contain', "A red light isn't the desired state in testing but it sure helps when riding your bike at night. Water-resistant with 3 lighting modes, 1 AAA battery included.")
         cy.get('[data-test="inventory-item-price"]').should('contain', '$9.99')
         cy.get('[data-test="add-to-cart"]').click()
-        cy.get('[data-test="shopping-cart-link"]').should('contain' ,'1')
+        cy.get('[data-test="shopping-cart-link"]').should('contain', '1')
         cy.get('[data-test="remove"]').click()
 
 
@@ -65,7 +77,7 @@ describe('sessions en Cypress', () => {
         cy.get('#shopping_cart_container > a > span').should('not.exist');
         /* ==== End Cypress Studio ==== */
     });
-    
-    
-    
+
+
+
 });
