8cffc00f91cfc32d24fda7259dc7f315b945e6de
Ruiz-Noelia <sagitrinidad@gmail.com>
2025-03-15 23:41:34 -0300

    Se crea test para subir archivos desde la computadora con resultado exitoso

---
+++ cypress/e2e/interacciones/uploadFile.cy.js
@@ -0,0 +1,17 @@
+describe('subir archivo', () => {
+
+    it('arrastro archivo', () => {
+        cy.visit('https://galiclerc.github.io/test-pages/pages/drag-file.html')
+
+        const dataTransfer = new DataTransfer();
+
+        //si el archivo se encuentra fuera de nuestracarpeta proyecto de cypress usamos sí o sí la ruta ABSOLUTA
+        cy.get('#dropArea').selectFile('C:/Users/ruizn/Downloads/bloque.png', { action: 'drag-drop' }) //indicamos donde soltamos nuestro archivo
+
+        // si el archivo a subir está dentro del proyecto nuestro de Cypress, lo subimos con la ruta relativa
+        cy.get('#dropArea').selectFile('cypress/downloads/cv_actualizado.pdf', { action: 'drag-drop' })
+
+
+    });
+
+})
