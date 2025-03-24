71bf2098f66680f2283ec7a09e90c33f59c62642
Noelia Ruiz <Ruiz Noelia>
2025-03-24 18:41:39 -0300

    comento subida de archivo con ruta absoluta por no encontrarse el archivo en en el entorno de ejecución de GitHub Actions

--- cypress/e2e/interacciones/uploadFile.cy.js
+++ cypress/e2e/interacciones/uploadFile.cy.js
@@ -6,7 +6,7 @@ describe('subir archivo', () => {
         const dataTransfer = new DataTransfer();
 
         //si el archivo se encuentra fuera de nuestracarpeta proyecto de cypress usamos sí o sí la ruta ABSOLUTA
-        cy.get('#dropArea').selectFile('c:/Users/pc/Downloads/bloque.png', { action: 'drag-drop' }) //indicamos donde soltamos nuestro archivo
+        //cy.get('#dropArea').selectFile('c:/Users/pc/Downloads/bloque.png', { action: 'drag-drop' }) //indicamos donde soltamos nuestro archivo
 
         // si el archivo a subir está dentro del proyecto nuestro de Cypress, lo subimos con la ruta relativa
         cy.get('#dropArea').selectFile('cypress/downloads/post.png', { action: 'drag-drop' })
